bb72ee13a1bc25da88b9fb777fce582c238aa874
cn.zzy <1971656621@qq.com>
2023-09-15 20:09:35 +0800

    init repository--first commit

---
+++ main-program-boot/src/main/java/cn/zzy/MainProgramBootApplication.java
@@ -0,0 +1,13 @@
+package cn.zzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainProgramBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainProgramBootApplication.class, args);
+    }
+
+}
---
+++ main-program-boot/src/test/java/cn/zzy/MainProgramBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProgramBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
